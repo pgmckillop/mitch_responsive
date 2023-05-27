5a70fec71309c4288b739aae02ffed942d2f33ea
Paul McKillop <pgmckillop@gmail.com>
2023-05-27 08:57:11 +0100

    Initial Commit

---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScaffold;
+  final Widget tabletScaffold;
+  final Widget desktopScaffold;
+
+  const ResponsiveLayout({
+    required this.mobileScaffold,
+    required this.tabletScaffold,
+    required this.desktopScaffold,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 800) {
+        return mobileScaffold;
+      } else if (constraints.maxWidth < 1100) {
+        return tabletScaffold;
+      } else {
+        return desktopScaffold;
+      }
+    });
+  }
+}
